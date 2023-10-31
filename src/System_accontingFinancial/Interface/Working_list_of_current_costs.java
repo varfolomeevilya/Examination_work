3bddf389259fe3d9b4c78aeda4e10724b56153de
илья <126683197+varfolomeevilya@users.noreply.github.com>
2023-10-31 12:32:54 +0300

    Update Working_list_of_current_costs.java

--- src/System_accontingFinancial/Interface/Working_list_of_current_costs.java
+++ src/System_accontingFinancial/Interface/Working_list_of_current_costs.java
@@ -1,12 +1,7 @@
 package System_accontingFinancial.Interface;
 
 public interface Working_list_of_current_costs {
-//   Working_list_of_current_costs(String str, int price){
-//       super(str, price);
-//       str="Купить одежду";
-//       price=
-//
-//   }
+
     void print(String str, double price);
 
 
